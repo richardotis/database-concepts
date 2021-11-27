1e67a475433873f12f83cb34465adb56cdab0229
Richard Otis <rotis@jpl.nasa.gov>
2021-11-27 14:51:44 -0800

    xml basic writing and validation

--- xml2/xml_parser.py
+++ xml2/xml_parser.py
@@ -1,7 +1,7 @@
 from pycalphad.io.tdb import _sympify_string
 from pycalphad import Database, variables as v
 from sympy import Piecewise, And, Symbol
-from lxml import etree
+from lxml import etree, objectify
 
 
 def convert_math_to_symbolic(math_nodes):
@@ -31,6 +31,28 @@ def convert_intervals_to_piecewise(interval_nodes):
         conds.append(cond)
         exprs.append(math_expr)
     return Piecewise(*(list(zip(exprs, conds)) + [(0, True)]))
+
+
+def convert_symbolic_to_nodes(sym):
+    nodes = []
+    if isinstance(sym, Piecewise):
+        for expr, cond in sym.args:
+            lower = ""
+            upper = ""
+            interval_node = etree.Element("Interval", attrib={"in": "T", "lower": lower, "upper": upper})
+            converted_expr_nodes = convert_symbolic_to_nodes(expr)
+            for node in converted_expr_nodes:
+                if isinstance(node, str):
+                    interval_node.text = node
+                else:
+                    interval_node.append(node)
+            if len(interval_node) == 0 and interval_node.text == '0':
+                continue
+            nodes.append(interval_node)
+
+    else:
+        nodes = [str(sym)]
+    return nodes
 
 
 def parse_parameter(param_node):
@@ -76,6 +98,9 @@ def read_xml(dbf, fd):
     parser = etree.XMLParser(load_dtd=False,
                              no_network=True)
     tree = etree.parse(fd, parser=parser)
+    relaxng = etree.RelaxNG(etree.parse('experiment3.rng'))
+    if not relaxng.validate(tree):
+        print('Validation Error:', relaxng.error_log.last_error)
     root = tree.getroot()
 
     for child in root:
@@ -97,4 +122,62 @@ def read_xml(dbf, fd):
     dbf.process_parameter_queue()
 
 
-Database.register_format("xml", read=read_xml, write=None)
+def write_xml(dbf, fd):
+    # TODO: metadata for writing database
+    root = objectify.Element("Database")
+    phase_nodes = {}
+    for element in sorted(dbf.elements):
+        objectify.SubElement(root, "ChemicalElement", id=str(element))
+    for species in sorted(dbf.species, key=lambda s: s.name):
+        if species.name not in dbf.elements:
+            # TODO
+            pass
+    for name, expr in sorted(dbf.symbols.items()):
+        expr_node = objectify.SubElement(root, "Expr", id=str(name))
+        converted_nodes = convert_symbolic_to_nodes(expr)
+        for node in converted_nodes:
+            if isinstance(node, str):
+                expr_node.text = node
+            else:
+                expr_node.append(node)
+    for name, phase_obj in sorted(dbf.phases.items()):
+        if phase_nodes.get(name, None) is None:
+            phase_nodes[name] = objectify.SubElement(root, "Phase", id=str(name))
+        # TODO: detection for MQMQA, etc.
+        if True:
+            model_node = objectify.SubElement(phase_nodes[name], "Model", type="CEF")
+            constit_array_node = objectify.SubElement(model_node, "ConstituentArray")
+            subl_idx = 0
+            for site_ratio, constituents in zip(phase_obj.sublattices, phase_obj.constituents):
+                site_node = objectify.SubElement(constit_array_node, "Site", id=str(subl_idx), ratio=str(site_ratio))
+                for constituent in constituents:
+                    objectify.SubElement(site_node, "Constituent", refid=str(constituent))
+                subl_idx += 1
+    for param in dbf._parameters.all():
+        phase_name = param['phase_name']
+        # Create phase implicitly if not defined
+        if phase_nodes.get(phase_name, None) is None:
+            phase_nodes[phase_name] = objectify.SubElement(root, "Phase", id=str(phase_name))
+        phase_node = phase_nodes[phase_name]
+        param_node = objectify.SubElement(phase_node, "Parameter", type=str(param['parameter_type']))
+        order_node = objectify.SubElement(param_node, "Order")
+        order_node._setText(str(param['parameter_order']))
+        constit_array_node = objectify.SubElement(param_node, "ConstituentArray")
+        subl_idx = 0
+        for constituents in param['constituent_array']:
+            site_node = objectify.SubElement(constit_array_node, "Site", refid=str(subl_idx))
+            for constituent in constituents:
+                objectify.SubElement(site_node, "Constituent", refid=str(constituent))
+            subl_idx += 1
+        nodes = convert_symbolic_to_nodes(param['parameter'])
+        for node in nodes:
+            if isinstance(node, str):
+                param_node._setText(node)
+            else:
+                param_node.append(node)
+        # TODO: param['diffusing_species']
+        # TODO: param['reference']
+    fd.write(etree.tostring(root, pretty_print=True).decode("utf-8"))
+
+
+Database.register_format("xml", read=read_xml, write=write_xml)
--- xml2/xml_test.py
+++ xml2/xml_test.py
@@ -5,3 +5,5 @@ dbf = Database('experiment3.xml')
 mod = Model(dbf, ['AL', 'ZN'], 'LIQUID')
 res = calculate(dbf, ['AL', 'ZN'], 'LIQUID', T=600, P=1e5)
 print(res)
+
+dbf.to_file('output_test.xml', if_exists="overwrite")
